38428ba960b614b838b34796038d32738c7e4f25
Dana Basken <dana@nanigans.com>
2018-04-08 16:06:20 -0400

    initial commit

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public float maxNodeDistance = 10.0f;
+    public float updateGraphSeconds = 2.0f;
+
+    private List<Node> nodes = new List<Node>();
+
+    void Start() {
+        AddNodeGameObjects();
+        StartCoroutine("UpdateGraphSolution");
+    }
+
+    void Update() {
+    }
+
+    void AddNodeGameObjects() {
+        GameObject[] nodeGameObjects = GameObject.FindGameObjectsWithTag("Node");
+        foreach (GameObject nodeGameObject in nodeGameObjects) {
+            AddNodeGameObject(nodeGameObject);
+        }
+    }
+
+    void AddNodeGameObject(GameObject nodeGameObject) {
+        Node node = nodeGameObject.GetComponent<Node>();
+        nodes.Add(node);
+    }
+
+    void ConnectGameObjects() {
+        foreach (Node node in nodes) {
+            node.Reset();
+        }
+        foreach (Node nodeA in nodes) {
+            foreach (Node nodeB in nodes) {
+                if (nodeA != nodeB) {
+                    float distance = Vector3.Distance(nodeA.transform.position, nodeB.transform.position);
+                    if (distance <= maxNodeDistance) {
+                        nodeA.AddConnection(nodeB);
+                        nodeB.AddConnection(nodeA);
+                        Debug.DrawLine(nodeA.transform.position, nodeB.transform.position, Color.red, updateGraphSeconds);
+                    }
+                }
+            }
+        }
+    }
+
+    List<Node>BuildPathList(Node end) {
+        List<Node> path = new List<Node>();
+        if (end.parent != null) {
+            Node node = end;
+            while (node != null) {
+                path.Add(node);
+                node = node.parent;
+            }
+        }
+        return path;
+    }
+
+    List<Node> FindPath(Node start, Node end) {
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(start);
+        float totalCost = start.SetCostAndParent(0.0f, null);
+        while (queue.Count > 0) {
+            Node currentNode = queue.Dequeue();
+            currentNode.visited = true;
+            Node leastCostNode = currentNode.GetLeastCostConnectedNode(totalCost);
+            if (leastCostNode != null) {
+                totalCost = leastCostNode.SetCostAndParent(totalCost, currentNode);
+            }
+            foreach (Node node in currentNode.GetConnectedNodes()) {
+                if (!node.visited) {
+                    queue.Enqueue(node);
+                    node.visited = true;
+                    node.parent = currentNode;
+                }
+            }
+        }
+        return BuildPathList(end);
+    }
+
+    IEnumerator UpdateGraphSolution() {
+        while (true) {
+            ConnectGameObjects();
+            Node start = null;
+            Node end = null;
+            foreach (Node node in nodes) {
+                if (node.isStartNode) {start = node;}
+                if (node.isEndNode) {end = node;}
+            }
+            if (start != null && end != null) {
+                List<Node> path = FindPath(start, end);
+                if (path != null && path.Count > 0) {
+                    Node lastNode = path[0];
+                    foreach (Node node in path) {
+                        Debug.DrawLine(lastNode.transform.position, node.transform.position, Color.green, updateGraphSeconds);
+                        lastNode = node;
+                    }
+                }
+            }
+            yield return new WaitForSeconds(updateGraphSeconds);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+	public bool isStartNode = false;
+	public bool isEndNode = false;
+
+    public Node parent = null;
+    public bool visited = false;
+
+	HashSet<Node> connectedNodes = new HashSet<Node>();
+
+	void Start() {
+	}
+	
+	void Update() {
+	}
+
+	public void AddConnection(Node node) {
+		connectedNodes.Add(node);
+	}
+
+	public void Reset() {
+		connectedNodes.Clear();
+        parent = null;
+        visited = false;
+	}
+
+	public HashSet<Node> GetConnectedNodes() {
+		return connectedNodes;
+	}
+
+    public float GetCost(Node node, float totalCost = 0.0f) {
+        if (node != null) {
+            return Vector3.Distance(transform.position, node.transform.position) + totalCost;
+        } else {
+            return totalCost;
+        }
+    }
+
+    public Node GetLeastCostConnectedNode(float totalCost) {
+        float leastCost = Mathf.Infinity;
+        Node leastCostNode = null;
+        foreach (Node node in connectedNodes) {
+            if (!node.visited) {
+                float cost = GetCost(node, totalCost);
+                if (cost < leastCost) {
+                    leastCost = cost;
+                    leastCostNode = node;
+                }
+            }
+        }
+        return leastCostNode;
+    }
+
+    public float SetCostAndParent(float totalCost, Node parent) {
+        this.parent = parent;
+        return GetCost(parent, totalCost);
+    }
+
+}
